081564bd80877e4ddd6da0bfe83eb02a21664891
Mohammad Kazem Khajeh <Kazem_kh95@hotmail.com>
2026-05-17 04:55:25 +0200

    chore: move constants to dedicated file

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+const GITHUB_OWNER = "MoaKK";
+const GITHUB_REPO = "npmstats";
+
+const NPM_DOWNLOADS_BASE = "https://api.npmjs.org";
+const NPM_REGISTRY_BASE = "https://registry.npmjs.org";
+
+export { GITHUB_OWNER, GITHUB_REPO, NPM_DOWNLOADS_BASE, NPM_REGISTRY_BASE };
